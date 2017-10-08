f0ec07e96dfc66a29fec7d931ad5a9b357bfdec8
Krygin Ivan <krygin.ia@gmail.com>
2017-10-09 00:17:19 +0300

    apk name

--- app/src/main/java/ru/krygin/smart_sight/DatabaseHelper.java
+++ app/src/main/java/ru/krygin/smart_sight/DatabaseHelper.java
@@ -19,8 +19,8 @@ import ru.krygin.smart_sight.snapshot.db.SnapshotData;
  */
 
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
-    private static int DATABASE_VERSION = 3;
-    private static String DATABASE_NAME = "ophtha_database.db";
+    private static int DATABASE_VERSION = 1;
+    private static String DATABASE_NAME = "smart_sight_database.db";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
